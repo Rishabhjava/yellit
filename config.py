37e8da31db62bbcd856c1ef5b5d757988851eaf4
Rishabhjava <40519476+Rishabhjava@users.noreply.github.com>
2023-05-01 12:01:47 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 BOT_TOKEN = "5653692437:AAGG84CedUAx1fh4s7qr78ADx7yGpl1mkos"
 API_ID = int(22464094)
 API_HASH = "068433765c898289e3a78ab67aa2230a"
-LOG_CHANNEL = int(-1823536404)
-AUTH_USERS = {'rishabhjava'}
-DB_URL = "mongodb+srv://rishabh:<FKqD4gCLb7BFQcnM>@cluster0.y8kmqmb.mongodb.net/test"
+LOG_CHANNEL = int(-1001823536404)
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1740687337").split())
+DB_URL = "mongodb+srv://rishabh:VuXxIVbr89JrdgAW@cluster0.y8kmqmb.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
